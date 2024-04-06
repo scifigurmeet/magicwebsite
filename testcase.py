efa306d642085f3d558693c59d08da780e14a13c
Gurmeet Singh <scifigurmeet@gmail.com>
2024-04-06 12:41:22 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import time
 
 # Start the WebDriver and open the HTML page
 service = Service(executable_path='/usr/local/bin/chromedriver')
