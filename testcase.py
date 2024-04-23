a3b9c41542317bfa2cde3537e6bea968bd6e7c86
scifigurmeet <56145753+scifigurmeet@users.noreply.github.com>
2024-04-23 16:37:50 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+import time
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Start the WebDriver and open the HTML page
 service = Service(executable_path='/usr/local/bin/chromedriver')
@@ -11,26 +13,53 @@ options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(service=service, options=options)
 
-# URL of your webpage
-URL = 'https://scifigurmeet.github.io/magicwebsite/'
-print(URL)
+# Open the WordPress login page
+driver.get("http://65.0.18.145/wp-login.php")
 
-try:
-    # Open the webpage
-    driver.get(URL)
+# Find the username field and enter the username
+username = driver.find_element(By.ID, "user_login")
+username.send_keys("user")
 
-    # Wait for the h1 element with text containing "Magic Website" to be present
-    h1_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "//h1[contains(., 'Gurmeet')]"))
-    )
+# Find the password field and enter the password
+password = driver.find_element(By.ID, "user_pass")
+password.send_keys("SyXXCMCWZe4Bo&ar1SafP$cU")
 
-    # If the element is found, print success message
-    print("Test Passed: Found h1 tag containing 'Magic Website'")
-except Exception as e:
-    # If the element is not found, print failure message
-    print("Test Failed:", e)
-    raise e
-finally:
-    # Close the WebDriver
-    driver.quit()
+# Find the login button and click it
+login_button = driver.find_element(By.ID, "wp-submit")
+login_button.click()
+
+driver.get("http://65.0.18.145/activity")
+
+commentBox = driver.find_element(By.ID, "whats-new")
+textToCheck = "Hello, this is the workshop by Xebia!"
+commentBox.send_keys(textToCheck)
+
+postUpdateButton = driver.find_element(By.ID, "aw-whats-new-submit")
+postUpdateButton.click()
+
+time.sleep(5)
+
+found_content = driver.find_element(
+    By.CSS_SELECTOR, '#activity-stream .activity-content .activity-inner > p').text
+
+assert found_content == textToCheck
+
+commentButton = driver.find_element(
+    By.CSS_SELECTOR, '#activity-stream .activity-content .acomment-reply')
+commentButton.click()
+
+actions = ActionChains(driver)
+
+# Simulate typing wherever the cursor is
+actions.send_keys("Your text to be typed")
+
+# Perform the actions
+actions.perform()
+
+commentPostButton = driver.find_element(
+    By.CSS_SELECTOR, '#activity-stream [type=submit]')
+commentPostButton.click()
+
+time.sleep(20)
+# Close the browser window
+driver.quit()
